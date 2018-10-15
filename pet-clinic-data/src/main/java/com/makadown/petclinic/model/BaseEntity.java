026a92725149f743d7d3f519063ba41921ad157f
Mario Serrano <maseflores79@gmail.com>
2018-10-15 09:18:15 -0700

    Agregando Entidad Base que contiene Id. Closes #10

---
+++ pet-clinic-data/src/main/java/com/makadown/petclinic/model/BaseEntity.java
@@ -0,0 +1,16 @@
+package com.makadown.petclinic.model;
+
+import java.io.Serializable;
+
+public class BaseEntity implements Serializable {
+
+    private Long Id;
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+}
