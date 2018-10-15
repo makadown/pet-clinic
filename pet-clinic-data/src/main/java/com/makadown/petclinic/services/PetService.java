1a0fc1c5a31bad2bf899d37d94124b96fb05834a
Mario Serrano <maseflores79@gmail.com>
2018-10-15 11:23:02 -0700

    Refactorizando a una interface comun de CRUD. Closes #11

--- pet-clinic-data/src/main/java/com/makadown/petclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/makadown/petclinic/services/PetService.java
@@ -2,14 +2,5 @@ package com.makadown.petclinic.services;
 
 import com.makadown.petclinic.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-
-    Pet findById(Long Id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
-    
+public interface PetService extends CrudService<Pet, Long>{
 }
