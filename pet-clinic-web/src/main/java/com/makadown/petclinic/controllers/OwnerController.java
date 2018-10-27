fc1992da117559f44ae6abf9fbc5b7a41ff1464d
Mario Serrano <maseflores79@gmail.com>
2018-10-27 10:48:05 -0700

    Aplicando el master layout a pagina principal de Owners. Closes #20

--- pet-clinic-web/src/main/java/com/makadown/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/makadown/petclinic/controllers/OwnerController.java
@@ -6,8 +6,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("/owners")
 @Controller
+@RequestMapping("/owners")
 public class OwnerController {
 
     private final OwnerService ownerService;
