ecd6b01e0c8fbc92f6c195b2acb20dd36a35778c
Mario Serrano <maseflores79@gmail.com>
2019-01-15 13:22:20 -0800

    Convirtiendo Vets a entidades JPA. Closes #37

--- pet-clinic-data/src/main/java/com/makadown/petclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/makadown/petclinic/model/Speciality.java
@@ -1,6 +1,12 @@
 package com.makadown.petclinic.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="specialties")
 public class Speciality extends BaseEntity {
+
+    @Column(name="description")
     private String description;
 
     public String getDescription() {
