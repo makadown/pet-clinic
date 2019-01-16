6c9b20bb48ee1731a9631a259b079b02e7315633
Mario Serrano <maseflores79@gmail.com>
2019-01-15 20:44:03 -0800

    Creando servicio JPA para Owner. Closes #40

--- pet-clinic-data/src/main/java/com/makadown/petclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/makadown/petclinic/repositories/OwnerRepository.java
@@ -4,4 +4,5 @@ import com.makadown.petclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+    Owner findByLastName(String lastname);
 }
---
+++ pet-clinic-data/src/main/java/com/makadown/petclinic/services/springdatajpa/OwnerSDJpaService.java
@@ -0,0 +1,61 @@
+package com.makadown.petclinic.services.springdatajpa;
+
+import com.makadown.petclinic.model.Owner;
+import com.makadown.petclinic.model.PetType;
+import com.makadown.petclinic.repositories.OwnerRepository;
+import com.makadown.petclinic.repositories.PetRepository;
+import com.makadown.petclinic.repositories.PetTypeRepository;
+import com.makadown.petclinic.services.OwnerService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class OwnerSDJpaService implements OwnerService {
+
+    private final OwnerRepository ownerRepository;
+    private final PetRepository petRepository;
+    private final PetTypeRepository petTypeRepository;
+
+    public OwnerSDJpaService(OwnerRepository ownerRepository, PetRepository petRepository, PetTypeRepository petTypeRepository) {
+        this.ownerRepository = ownerRepository;
+        this.petRepository = petRepository;
+        this.petTypeRepository = petTypeRepository;
+    }
+
+    @Override
+    public Owner findByLastName(String lastName) {
+        return ownerRepository.findByLastName(lastName);
+    }
+
+    @Override
+    public Set<Owner> findAll() {
+        Set<Owner> owners = new HashSet<>();
+        // en una sola linea hace un foreach para agregar todos los elementos de findAll() en owners
+        ownerRepository.findAll().forEach(owners::add);
+        return owners;
+    }
+
+    @Override
+    public Owner findById(Long aLong) {
+        return ownerRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return ownerRepository.save(object);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        ownerRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        ownerRepository.deleteById(aLong);
+    }
+}
