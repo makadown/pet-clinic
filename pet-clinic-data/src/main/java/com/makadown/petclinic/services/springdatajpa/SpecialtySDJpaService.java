15483fde4b58e87fc76048e01e2a5af5d38381d1
Mario Serrano <maseflores79@gmail.com>
2019-01-16 12:54:36 -0800

    Creado servicio JPA para Speciality. Closes #44

---
+++ pet-clinic-data/src/main/java/com/makadown/petclinic/services/springdatajpa/SpecialtySDJpaService.java
@@ -0,0 +1,47 @@
+package com.makadown.petclinic.services.springdatajpa;
+
+import com.makadown.petclinic.model.Speciality;
+import com.makadown.petclinic.repositories.SpecialtyRepository;
+import com.makadown.petclinic.services.SpecialtyService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class SpecialtySDJpaService implements SpecialtyService {
+    private final SpecialtyRepository specialtyRepository;
+
+    public SpecialtySDJpaService(SpecialtyRepository specialtyRepository) {
+        this.specialtyRepository = specialtyRepository;
+    }
+
+    @Override
+    public Set<Speciality> findAll() {
+        Set<Speciality> specialties = new HashSet<>();
+        specialtyRepository.findAll().forEach(specialties::add);
+        return specialties;
+    }
+
+    @Override
+    public Speciality findById(Long aLong) {
+        return specialtyRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return specialtyRepository.save(object);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        specialtyRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        specialtyRepository.deleteById(aLong);
+    }
+}
