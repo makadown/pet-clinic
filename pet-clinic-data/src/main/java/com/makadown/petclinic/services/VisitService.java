0dcaf0ba2cc289ffda39ae20a07801f5b1d0d299
Mario Serrano <maseflores79@gmail.com>
2019-01-17 19:04:25 -0800

    Creado Visit Map Service. Closes #46

---
+++ pet-clinic-data/src/main/java/com/makadown/petclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package com.makadown.petclinic.services;
+
+import com.makadown.petclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/makadown/petclinic/services/map/VisitMapService.java
@@ -0,0 +1,4 @@
+package com.makadown.petclinic.services.Map;
+
+public class VisitMapService {
+}
